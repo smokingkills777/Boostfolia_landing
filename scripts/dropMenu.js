1db31dc01218bff35d4c0dbb33a39cc365c69295
SmokingKills777 <greenboy702@gmail.com>
2021-01-28 16:57:00 +0200

    menu fixed

---
+++ scripts/dropMenu.js
@@ -0,0 +1,19 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("activeMenu");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('activeMenu')) {
+        openDropdown.classList.remove('activeMenu');
+      }
+    }
+  } else {
+    openDropdown.classList.remove('show');
+  }
+}
